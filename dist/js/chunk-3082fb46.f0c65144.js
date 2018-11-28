a36fbe83bcddd1fb53fdf8fc236252ae51d73fd5
neo-mac <miaobingliang@neotrident.com>
2018-11-28 14:10:27 +0800

    大宝

--- dist/js/chunk-3082fb46.f0c65144.js
+++
@@ -1,2 +0,0 @@
-(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-3082fb46"],{"685e":function(e,n,u){},"8f04":function(e,n,u){"use strict";u.r(n);var t=function(){var e=this,n=e.$createElement,u=e._self._c||n;return u("div",[u("Card",[u("h2",[e._v("ID: "+e._s(e.$route.query.id))])])],1)},r=[],i={name:"query"},c=i,o=(u("d187"),u("048f")),s=Object(o["a"])(c,t,r,!1,null,null,null);s.options.__file="query.vue";n["default"]=s.exports},d187:function(e,n,u){"use strict";var t=u("685e"),r=u.n(t);r.a}}]);
-//# sourceMappingURL=chunk-3082fb46.f0c65144.js.map
