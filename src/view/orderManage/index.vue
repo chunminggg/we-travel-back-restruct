8516093553eb45c2c3e765379131c975baaf592c
zhangzhi <zhi.zhang@chivox.com>
2018-11-29 11:28:12 +0800

    订单页复制

--- src/view/orderManage/index.vue
+++ src/view/orderManage/index.vue
@@ -1,24 +1,61 @@
 <template>
   <div>
     <div>
-      <Button type="primary" @click="addDestination">添加订单</Button>
+      <Button
+        type="primary"
+        @click="addDestination"
+      >添加订单</Button>
     </div>
     <div style="margin-top:10px;">
-      <Table :columns="columns" :data="data"></Table>
+      <Table
+        :columns="columns"
+        :data="data"
+      ></Table>
     </div>
-    <Modal title="添加目的地" v-model="isShowAddModal" @on-ok="confirmaddDestination">
-      <Input v-model="addName" placeholder="输入目的地名称"></Input>
-      <Select v-model="relateId" placeholder="关联分类" clearable style="margin-top:10px">
-        <Option v-for="item in themeData" :key="item.id" :value="item.id">
-          {{item.attributes.name}}
+    <Modal
+      title="添加订单"
+      v-model="isShowAddModal"
+      @on-ok="confirmaddDestination"
+    >
+      <Input
+        v-model="addName"
+        placeholder="输入订单名称"
+      ></Input>
+      <Select
+        v-model="relateId"
+        placeholder="关联分类"
+        clearable
+        style="margin-top:10px"
+      >
+        <Option
+          v-for="item in themeData"
+          :key="item.objectId"
+          :value="item.objectId"
+        >
+          {{item.name}}
         </Option>
       </Select>
     </Modal>
-    <Modal title="编辑目的地" v-model="isShowEditModal" @on-ok="confirmeditDestination">
-      <Input v-model="editName" placeholder="输入目的地名称"></Input>
-      <Select v-model="relateId" clearable style="margin-top:10px">
-        <Option v-for="item in themeData" :key="item.id" :value="item.id">
-          {{item.attributes.name}}
+    <Modal
+      title="编辑订单"
+      v-model="isShowEditModal"
+      @on-ok="confirmeditDestination"
+    >
+      <Input
+        v-model="editName"
+        placeholder="输入订单名称"
+      ></Input>
+      <Select
+        v-model="relateId"
+        clearable
+        style="margin-top:10px"
+      >
+        <Option
+          v-for="item in themeData"
+          :key="item.objectId"
+          :value="item.objectId"
+        >
+          {{item.name}}
         </Option>
       </Select>
     </Modal>
@@ -32,21 +69,23 @@ import {
   deleteDestination,
   editDestination,
   getTheme
-} from "@/libs/service";
+} from '@/libs/service'
 export default {
   data() {
     return {
-      relateId: "",
-      editRelateId: "",
-      data: [],
-      editName: "",
+      relateId: '',
+      editRelateId: '',
+      data: [{
+        a: 1
+      }],
+      editName: '',
       themeData: [],
       isShowEditModal: false,
       columns: [
         {
-          title: "名称",
+          title: '名称',
           render: (h, params) => {
-            return <span>{params.row.attributes.name}</span>;
+            return <span>{params.row.name}</span>
           }
         },
         {
@@ -54,23 +93,23 @@ export default {
           render: (h, params) => {
             let name = ''
             this.themeData.map(item => {
-              if (item.id == params.row.attributes.targetType.id) {
-                name = item.attributes.name
+              if (item.objectId == params.row.targetType.objectId) {
+                name = item.name
               }
             })
             return (<span>{name}</span>)
           }
         },
         {
-          title: "操作",
-          key: "action",
+          title: '操作',
+          key: 'action',
           render: (h, params) => {
             return (
               <div>
                 <i-button
                   type="error"
                   onClick={() => {
-                    this.deleteDestination(params.row.id);
+                    this.deleteDestination(params.row.objectId)
                   }}
                 >
                   删除
@@ -79,68 +118,68 @@ export default {
                   type="primary"
                   style="margin-left:10px"
                   onClick={() => {
-                    this.showEditModal(params);
+                    this.showEditModal(params)
                   }}
                 >
                   编辑
                 </i-button>
               </div>
-            );
+            )
           }
         }
       ],
       isShowAddModal: false,
-      addName: "",
+      addName: '',
       currentId: null
-    };
+    }
   },
   mounted() {
-    this.getThemeData();
-    this.getData();
+    this.getThemeData()
+    this.getData()
   },
   methods: {
     async getThemeData() {
-      let data = await getTheme();
-      this.themeData = data;
-      this.relateId = this.themeData[0].id
+      let data = await getTheme()
+      this.themeData = data.map(v => v.toJSON())
+      this.relateId = this.themeData[0].objectId
     },
     showEditModal(params) {
-      this.isShowEditModal = true;
-      this.currentId = params.row.id;
-      this.editRelateId = params.row.attributes.targetType.id
-      this.editName = params.row.attributes.name;
+      this.isShowEditModal = true
+      this.currentId = params.row.objectId
+      this.editRelateId = params.row.targetType.objectId
+      this.editName = params.row.name
     },
     addDestination() {
-      this.isShowAddModal = true;
+      this.isShowAddModal = true
     },
     async confirmeditDestination() {
-      let data = await editDestination(this.editName, this.currentId, this.editRelateId);
-      this.$Message.success("修改成功");
-      this.getData();
+      let data = await editDestination(this.editName, this.currentId, this.editRelateId)
+      this.$Message.success('修改成功')
+      this.getData()
     },
     async confirmaddDestination() {
-      let data = await addDestination(this.addName, this.relateId);
-      this.$Message.success("添加成功");
-      this.addName = "";
-      this.getData();
+      let data = await addDestination(this.addName, this.relateId)
+      this.$Message.success('添加成功')
+      this.addName = ''
+      this.getData()
     },
     async getData() {
-      let data = await getDestination();
-      this.data = data;
+      let data = await getDestination()
+      this.data = data.map(v => v.toJSON())
     },
-    async deleteDestination(id) {
+    async deleteDestination(objectId) {
       this.$Modal.confirm({
-        content: "确认删除吗",
+        content: '确认删除吗',
         onOk: () => {
-          deleteDestination(id).then(res => {
-            this.getData();
-            this.$Message.success("删除成功");
-          });
+          deleteDestination(objectId).then(res => {
+            this.getData()
+            this.$Message.success('删除成功')
+          })
         }
-      });
+      })
     }
   }
-};
+}
 </script>
 
 <style>
