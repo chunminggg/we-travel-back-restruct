5e9f55dbca963dc065170bb75ce11338aa45153c
zhangzhi <zhi.zhang@chivox.com>
2018-11-29 11:02:40 +0800

    目的地管理，修改报错

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,16 +1,21 @@
 module.exports = {
   root: true,
+  env: {
+    node: true
+  },
   'extends': [
     'plugin:vue/essential',
-    '@vue/standard'
+    // 'eslint:recommended'
   ],
   rules: {
-    // allow async-await
-    'generator-star-spacing': 'off',
-    // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'vue/no-parsing-error': [2, { 'x-invalid-end-tag': false }],
-    'no-undef': 'off'
+    'vue/no-parsing-error': [2, {
+      'x-invalid-end-tag': false
+    }],
+    'vue/require-v-for-key': false,
+    'vue/no-side-effects-in-computed-properties': 'off',
+    'vue/no-async-in-computed-properties': false,
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
     parser: 'babel-eslint'
--- src/libs/service.js
+++ src/libs/service.js
@@ -1,14 +1,14 @@
-import AV from 'leancloud-storage';
-var APP_ID = 'CoowAeop80CKNvkcGJow83RN-gzGzoHsz';
-var APP_KEY = 'mVNul0fg5zl0bNdrEFerfE2Y';
+import AV from 'leancloud-storage'
+var APP_ID = 'CoowAeop80CKNvkcGJow83RN-gzGzoHsz'
+var APP_KEY = 'mVNul0fg5zl0bNdrEFerfE2Y'
 AV.init({
   appId: APP_ID,
   appKey: APP_KEY
-});
+})
 export const addTheme = (name) => {
-  const Product =  AV.Object.extend('Type')
+  const Product = AV.Object.extend('Type')
   let product = new Product()
-  product.set('name',name)
+  product.set('name', name)
   return product.save()
 }
 export const getTheme = () => {
@@ -16,22 +16,22 @@ export const getTheme = () => {
   return theme.find()
 }
 export const deleteTheme = (id) => {
-    let item = AV.Object.createWithoutData('Type',id)
-    return item.destroy()
+  let item = AV.Object.createWithoutData('Type', id)
+  return item.destroy()
 }
-export const editTheme = (name,id,targetId) => {
-  let item = AV.Object.createWithoutData('Type',id)
-  item.set('name',name)
-  let targetTheme = AV.Object.createWithoutData('Type',targetId)
-  item.set('targetType',targetTheme)
+export const editTheme = (name, id, targetId) => {
+  let item = AV.Object.createWithoutData('Type', id)
+  item.set('name', name)
+  let targetTheme = AV.Object.createWithoutData('Type', targetId)
+  item.set('targetType', targetTheme)
   return item.save()
 }
-export const addDestination = (name,id) => {
-  const Product =  AV.Object.extend('Destination')
+export const addDestination = (name, id) => {
+  const Product = AV.Object.extend('Destination')
   let product = new Product()
-  product.set('name',name)
-  let targetTheme = AV.Object.createWithoutData('Type',id)
-  product.set('targetType',targetTheme)
+  product.set('name', name)
+  let targetTheme = AV.Object.createWithoutData('Type', id)
+  product.set('targetType', targetTheme)
   return product.save()
 }
 export const getDestination = () => {
@@ -39,12 +39,12 @@ export const getDestination = () => {
   return theme.find()
 }
 export const deleteDestination = (id) => {
-    let item = AV.Object.createWithoutData('Destination',id)
-    return item.destroy()
+  let item = AV.Object.createWithoutData('Destination', id)
+  return item.destroy()
 }
-export const editDestination = (name,id) => {
-  let item = AV.Object.createWithoutData('Destination',id)
-  item.set('name',name)
+export const editDestination = (name, id) => {
+  let item = AV.Object.createWithoutData('Destination', id)
+  item.set('name', name)
   return item.save()
 }
 export const userLogin = (userName, password) => {
@@ -52,22 +52,22 @@ export const userLogin = (userName, password) => {
 }
 export const uploadFile = (file) => {
   let name = file.name,
-    dataFile = new AV.File(name, file);
+    dataFile = new AV.File(name, file)
   return dataFile.save()
 }
 
 export const createNewProduct = (data) => {
-  var Product = AV.Object.extend('Product');
-  var product = new Product();
-  product.set('startDate', data.startDate);
-  product.set('name', data.name);
-  product.set('describe', data.describe);
-  product.set('type', data.type);
-  product.set('place', data.place);
-  product.set('onleyId', data.onleyId);
-  product.set('price', data.price);
-  product.set('imageArray', data.imageArray);
-  product.set('detailContent', data.detailContent);
+  var Product = AV.Object.extend('Product')
+  var product = new Product()
+  product.set('startDate', data.startDate)
+  product.set('name', data.name)
+  product.set('describe', data.describe)
+  product.set('type', data.type)
+  product.set('place', data.place)
+  product.set('onleyId', data.onleyId)
+  product.set('price', data.price)
+  product.set('imageArray', data.imageArray)
+  product.set('detailContent', data.detailContent)
   product.set('isRecommend', data.isRecommend)
   product.set('isSpecialPrice', data.isSpecialPrice)
   product.set('isFreeTravel', data.isFreeTravel)
@@ -78,38 +78,36 @@ export const createNewProduct = (data) => {
   product.set('isNewProduct', data.isNewProduct)
   product.set('fileArray', data.fileArray)
   return product.save()
-
 }
 export const getProductList = (params) => {
-  var query = new AV.Query('Product');
-  query.select(['place', 'name', 'startDate', 'type', 'endDate', 'onleyId', 'price', 'describe', 'imageArray', 'countNumber']);
+  var query = new AV.Query('Product')
+  query.select(['place', 'name', 'startDate', 'type', 'endDate', 'onleyId', 'price', 'describe', 'imageArray', 'countNumber'])
   query.addDescending('countNumber')
-  query.select(['isSort', 'isFreeTravel', 'isRecommend', 'isFollowTeam', 'isSpecialPrice', 'place', 'name', 'startDate', 'type', 'endDate', 'onleyId', 'price', 'describe', 'imageArray', 'countNumber']);
-  query.addDescending('countNumber');
+  query.select(['isSort', 'isFreeTravel', 'isRecommend', 'isFollowTeam', 'isSpecialPrice', 'place', 'name', 'startDate', 'type', 'endDate', 'onleyId', 'price', 'describe', 'imageArray', 'countNumber'])
+  query.addDescending('countNumber')
   query.addDescending('updatedAt')
   return query.find()
 }
 export const deleteProduct = (productId) => {
-  var todo = AV.Object.createWithoutData('Product', productId);
+  var todo = AV.Object.createWithoutData('Product', productId)
   return todo.destroy()
 }
 export const getProductDetail = (productId) => {
-
-  var query = new AV.Query('Product');
+  var query = new AV.Query('Product')
   return query.get(productId)
 }
-export const saveEditProduct = (data) =>{
-  let Product = AV.Object.extend('Product');
-  let product = AV.Object.createWithoutData('Product', data.id);
-  product.set('startDate', data.startDate);
-  product.set('name', data.name);
-  product.set('describe', data.describe);
-  product.set('type', data.type);
-  product.set('place', data.place);
-  product.set('onleyId', data.onleyId);
-  product.set('price', data.price);
-  product.set('imageArray', data.imageArray);
-  product.set('detailContent', data.detailContent);
+export const saveEditProduct = (data) => {
+  let Product = AV.Object.extend('Product')
+  let product = AV.Object.createWithoutData('Product', data.id)
+  product.set('startDate', data.startDate)
+  product.set('name', data.name)
+  product.set('describe', data.describe)
+  product.set('type', data.type)
+  product.set('place', data.place)
+  product.set('onleyId', data.onleyId)
+  product.set('price', data.price)
+  product.set('imageArray', data.imageArray)
+  product.set('detailContent', data.detailContent)
   product.set('isRecommend', data.isRecommend)
   product.set('isSpecialPrice', data.isSpecialPrice)
   product.set('isFreeTravel', data.isFreeTravel)
