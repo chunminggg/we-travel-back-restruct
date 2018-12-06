9e3cb35d513036d5690af8a50783968462cabec3
neo-mac <miaobingliang@neotrident.com>
2018-12-06 12:06:11 +0800

    添加价格成功

--- src/components/calendar/index.vue
+++ src/components/calendar/index.vue
@@ -1,7 +1,7 @@
 <template>
 
     <div>
-        <calendar ref="calendar" style="height: 800px;" view="month" @afterRenderSchedule="onAfterRenderSchedule" @beforeCreateSchedule="onBeforeCreateSchedule" @beforeDeleteSchedule="onBeforeDeleteSchedule" @beforeUpdateSchedule="onBeforeUpdateSchedule" @clickDayname="onClickDayname" @clickSchedule="onClickSchedule" @clickTimezonesCollapseBtn="onClickTimezonesCollapseBtn" :schedules="scheduleList" :scheduleView="scheduleView" />
+        <calendar ref="calendar" style="height: 800px;" :useCreationPopup="isShowPopup" :useDetailPopup="isShowPopup" view="month" @afterRenderSchedule="onAfterRenderSchedule" @beforeCreateSchedule="onBeforeCreateSchedule" @beforeDeleteSchedule="onBeforeDeleteSchedule" @beforeUpdateSchedule="onBeforeUpdateSchedule" @clickDayname="onClickDayname" @clickSchedule="onClickSchedule" @clickTimezonesCollapseBtn="onClickTimezonesCollapseBtn" :schedules="scheduleList" :scheduleView="scheduleView" />
     </div>
 </template>
 
@@ -15,6 +15,7 @@ export default {
   data() {
     return {
       scheduleView: ["time"],
+      isShowPopup: false,
       calendarList: [
         {
           id: "0",
@@ -25,64 +26,46 @@ export default {
           name: "office"
         }
       ],
-      scheduleList: [
-        {
-          id: "1",
-          calendarId: "1",
-          title: "my schedule",
-          category: "time",
-          dueDateClass: "",
-          start: "2018-12-18T22:30:00+09:00",
-          end: "2018-12-19T02:30:00+09:00"
-        },
-        {
-          id: "2",
-          calendarId: "1",
-          title: "second schedule",
-          category: "time",
-          dueDateClass: "",
-          start: "2018-12-18T17:30:00+09:00",
-          end: "2018-12-19T17:31:00+09:00"
-        }
-      ]
+      scheduleList: [],
+      currentDateItem: {}
     };
   },
   mounted() {},
   methods: {
-    onAfterRenderSchedule(e) {
-      // implement your code
-      debugger;
-    },
-    onBeforeCreateSchedule(e) {
+    addSchedult() {
       let params = {
         id: this.scheduleList.length + 1,
         calendarId: "1",
         category: "time",
         dueDateClass: "",
-        start: e.start,
-        title: e.title
+        start: this.currentDateItem.start,
+        title: "33333"
       };
       this.scheduleList.push(params);
     },
+    onAfterRenderSchedule(e) {
+      // implement your code
+    },
+    onBeforeCreateSchedule(e) {
+      this.currentDateItem = e;
+      e.guide.clearGuideElement();
+      this.$parent.priceModal = true;
+      return;
+    },
     onBeforeDeleteSchedule(e) {
       // implement your code
-      debugger;
     },
     onBeforeUpdateSchedule(e) {
       // implement your code
-      debugger;
     },
     onClickDayname(e) {
       // implement your code
-      debugger;
     },
     onClickSchedule(e) {
       // implement your code
-      debugger;
     },
     onClickTimezonesCollapseBtn(e) {
       // implement your code
-      debugger;
     }
   }
 };
--- src/view/components/edit-product/edit-product.vue
+++ src/view/components/edit-product/edit-product.vue
@@ -16,92 +16,91 @@
   <div class="content">
     <div v-if="!isShowAddPriceView">
 
-   
-    <h2>产品发布</h2>
-    <Input v-model="productNumber" placeholder="" class="product">
-    <span slot="prepend">产品编号</span>
-    </Input>
-    <Input v-model="productName" placeholder="" class="product">
-    <span slot="prepend">产品名称</span>
-    </Input>
-    <Input v-model="productPlace" placeholder="" class="product">
-    <span slot="prepend">出发城市</span>
-    </Input>
-    <Input v-model="productPrice" placeholder="" class="product">
-    <span slot="prepend">产品价格</span>
-    </Input>
-    <Input v-model="productDes" placeholder="" class="product">
-    <span slot="prepend">产品描述</span>
-    </Input>
-    <Input v-model="productStartDate" placeholder="" class="product">
-    <span slot="prepend">开始时间</span>
-    </Input>
-    <div class="product">
-      <Button type="info" class="product" @click="priceSelect">价格添加</Button>
-      <div class="price-card">
-        <Row :gutter="16">
-          <Col span="6" v-for="(item,index) in tagArray" :key="index">
-          <Card>
+      <h2>产品发布</h2>
+      <Input v-model="productNumber" placeholder="" class="product">
+      <span slot="prepend">产品编号</span>
+      </Input>
+      <Input v-model="productName" placeholder="" class="product">
+      <span slot="prepend">产品名称</span>
+      </Input>
+      <Input v-model="productPlace" placeholder="" class="product">
+      <span slot="prepend">出发城市</span>
+      </Input>
+      <Input v-model="productPrice" placeholder="" class="product">
+      <span slot="prepend">产品价格</span>
+      </Input>
+      <Input v-model="productDes" placeholder="" class="product">
+      <span slot="prepend">产品描述</span>
+      </Input>
+      <Input v-model="productStartDate" placeholder="" class="product">
+      <span slot="prepend">开始时间</span>
+      </Input>
+      <div class="product">
+        <Button type="info" class="product" @click="priceSelect">价格添加</Button>
+        <div class="price-card">
+          <Row :gutter="16">
+            <Col span="6" v-for="(item,index) in tagArray" :key="index">
+            <Card>
               <div slot="extra">
                 <Button type="error" @click="handleClose(index)">删除</Button>
                 <Button type="primary" style="margin-left:10px;" @click="handleEditPrice(item)">编辑</Button>
               </div>
-            <div>
-              成人票价:{{item.price}}
-            </div>
-            <div>
-              儿童票价:{{item.childPrice}}
-            </div>
-             <div>
-              备注:{{item.comment}}
-            </div>
-            <div>
-              <div>开始日期：{{item.startDate}}</div>
-              <div>结束日期：{{item.endDate}}</div>
-            </div>
-          </Card>
+              <div>
+                成人票价:{{item.price}}
+              </div>
+              <div>
+                儿童票价:{{item.childPrice}}
+              </div>
+              <div>
+                备注:{{item.comment}}
+              </div>
+              <div>
+                <div>开始日期：{{item.startDate}}</div>
+                <div>结束日期：{{item.endDate}}</div>
+              </div>
+            </Card>
 
-          </Col>
-        </Row>
+            </Col>
+          </Row>
+        </div>
       </div>
+      <div class="product">
+        <label>是否特价:</label>
+        <i-switch v-model="isRecommend"></i-switch>
+        <label>是否推荐:</label>
+        <i-switch v-model="isSpecialPrice"></i-switch>
+        <label>是否跟团游</label>
+        <i-switch v-model="isFollowTeam"></i-switch>
+        <label>是否自由行</label>
+        <i-switch v-model="isFreeTravel"></i-switch>
+      </div>
+      <Select v-model="productTypeSelected" class="product" placeholder="请选择产品类型">
+        <Option v-for="(item,index) in productTypes" :value="item.value" :key="index">{{ item.label }}</Option>
+      </Select>
+      <Alert class="myAlert" type="warning">产品详情滚动图（至少传一张)</Alert>
+      <image-upload class="product imageUpload" @send-image="getImageArray" :uploadList="imageArray">
+      </image-upload>
+      <div v-for="(richItem, index) in richItems" :key="index" class="myProduct">
+        <Alert>{{richItem.placeHolder}}</Alert>
+        <!-- <quill-editor v-model="richItem.content"></quill-editor> -->
+        <rich-editor :ref="'rich'+index" :idx="index"></rich-editor>
+      </div>
+      <Button type="success" long @click="submitData" class="product">确认提交</Button>
+      <Button type="error" long @click="elseSubmitData" class="product">另存为</Button>
     </div>
-    <div class="product">
-      <label>是否特价:</label>
-      <i-switch v-model="isRecommend"></i-switch>
-      <label>是否推荐:</label>
-      <i-switch v-model="isSpecialPrice"></i-switch>
-      <label>是否跟团游</label>
-      <i-switch v-model="isFollowTeam"></i-switch>
-      <label>是否自由行</label>
-      <i-switch v-model="isFreeTravel"></i-switch>
+    <div v-if="isShowAddPriceView">
+      <div>
+        <Button @click="isShowAddPriceView = false">返回</Button>
+      </div>
+      <price-view ref="holePriceView"></price-view>
     </div>
-    <Select v-model="productTypeSelected" class="product" placeholder="请选择产品类型">
-      <Option v-for="(item,index) in productTypes" :value="item.value" :key="index">{{ item.label }}</Option>
-    </Select>
-    <Alert class="myAlert" type="warning">产品详情滚动图（至少传一张)</Alert>
-    <image-upload class="product imageUpload" @send-image="getImageArray" :uploadList="imageArray">
-    </image-upload>
-    <div v-for="(richItem, index) in richItems" :key="index" class="myProduct">
-      <Alert>{{richItem.placeHolder}}</Alert>
-      <!-- <quill-editor v-model="richItem.content"></quill-editor> -->
-      <rich-editor :ref="'rich'+index" :idx="index"></rich-editor>
-    </div>
-    <Button type="success" long @click="submitData" class="product">确认提交</Button>
-    <Button type="error" long @click="elseSubmitData" class="product">另存为</Button>
- </div>
- <div v-if="isShowAddPriceView">
-   <div>
-     <Button @click="isShowAddPriceView = false">返回</Button>
-   </div>
-<price-view></price-view>
- </div>
     <Modal v-model="priceModal" title="价格添加" @on-ok="priceAdd">
       <Input v-model="singlePrice" placeholder="请输入成人价格" style="width: 300px"></Input>
       <Input v-model="singleChildPrice" placeholder="请输入儿童价格" style="width: 300px" class="product"></Input>
-       <Input v-model="singlePriceComment" placeholder="请输入备注" style="width: 300px" class="product"></Input>
+      <Input v-model="singlePriceComment" placeholder="请输入备注" style="width: 300px" class="product"></Input>
       <DatePicker v-model="singleDate" placeholder="选择日期" style="width: 300px" type="daterange" class="product"></DatePicker>
     </Modal>
-  <price-form ref="priceForm" @priceEdit="priceEdit"></price-form>
+    <price-form ref="priceForm" @priceEdit="priceEdit"></price-form>
   </div>
 </template>
 
@@ -117,8 +116,8 @@ import { quillEditor } from "vue-quill-editor";
 import { saveEditProduct, getProductDetail, getTheme } from "@/libs/service";
 import moment from "moment";
 import richEditor from "@/components/productEditor/editor";
-import priceForm from '@/components/product/priceForm'
-import priceView from '@/components/calendar/index'
+import priceForm from "@/components/product/priceForm";
+import priceView from "@/components/calendar/index";
 export default {
   components: {
     imageUpload,
@@ -130,7 +129,7 @@ export default {
   },
   data() {
     return {
-      isShowAddPriceView:false,
+      isShowAddPriceView: false,
       richItems: [
         { content: "", placeHolder: "线路特色" },
         { content: "", placeHolder: "行程介绍" },
@@ -174,15 +173,12 @@ export default {
           return date && date.valueOf() < Date.now() - 86400000;
         }
       },
-      productTypes: [
-      ],
+      productTypes: [],
       productTypeSelected: "",
       imageArray: [],
       fileArray: [],
-      singlePriceComment:'',
-      singleChildPrice:'',
-
-
+      singlePriceComment: "",
+      singleChildPrice: ""
     };
   },
   created() {
@@ -191,8 +187,8 @@ export default {
     this.getData(this.productId);
   },
   methods: {
-    handleEditPrice(item){
-      this.$refs.priceForm.configData(item)
+    handleEditPrice(item) {
+      this.$refs.priceForm.configData(item);
     },
     async getThemeData() {
       let data = await getTheme();
@@ -247,30 +243,32 @@ export default {
     },
     // 价格添加
     priceAdd() {
+      this.$refs.holePriceView.addSchedult()
+      return
       let dict = {
         startDate: moment(this.singleDate[0]).format("YYYY-MM-DD"),
         endDate: moment(this.singleDate[1]).format("YYYY-MM-DD"),
         price: this.singlePrice,
         childPrice: this.singleChildPrice,
-        comment:this.singlePriceComment
+        comment: this.singlePriceComment
       };
 
       this.tagArray.push(dict);
     },
-    priceEdit(params,index){
-       let dict = {
+    priceEdit(params, index) {
+      let dict = {
         startDate: moment(params.dateRange[0]).format("YYYY-MM-DD"),
         endDate: moment(params.dateRange[1]).format("YYYY-MM-DD"),
         price: params.price,
         childPrice: params.childPrice,
-        comment:params.comment
+        comment: params.comment
       };
-      this.$set(this.tagArray,index,dict)
-      this.tagArray[index] = dict
+      this.$set(this.tagArray, index, dict);
+      this.tagArray[index] = dict;
     },
     //价格选择
     priceSelect() {
-      this.isShowAddPriceView = true
+      this.isShowAddPriceView = true;
       // this.priceModal = true;
     },
     getRichTextArray(data) {
@@ -337,7 +335,7 @@ export default {
 
 
 <style scoped lang="less">
-.price-card{
-  margin-top:10px;
+.price-card {
+  margin-top: 10px;
 }
 </style>
