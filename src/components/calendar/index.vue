ef43fd053602d8153f1e486578acd093bee3af5c
neo-mac <miaobingliang@neotrident.com>
2018-12-06 09:10:48 +0800

    日历加价格

--- src/components/calendar/index.vue
+++ src/components/calendar/index.vue
@@ -1,7 +1,7 @@
 <template>
 
     <div>
-        <calendar ref="calendar" style="height: 800px;" view="month" @clickDayname="onClickDayname" @clickSchedule="onClickSchedule" />
+        <calendar ref="calendar" style="height: 800px;" view="month" @afterRenderSchedule="onAfterRenderSchedule" @beforeCreateSchedule="onBeforeCreateSchedule" @beforeDeleteSchedule="onBeforeDeleteSchedule" @beforeUpdateSchedule="onBeforeUpdateSchedule" @clickDayname="onClickDayname" @clickSchedule="onClickSchedule" @clickTimezonesCollapseBtn="onClickTimezonesCollapseBtn" :schedules="scheduleList" :scheduleView="scheduleView" />
     </div>
 </template>
 
@@ -14,17 +14,76 @@ export default {
   },
   data() {
     return {
+      scheduleView: ["time"],
+      calendarList: [
+        {
+          id: "0",
+          name: "home"
+        },
+        {
+          id: "1",
+          name: "office"
+        }
+      ],
+      scheduleList: [
+        {
+          id: "1",
+          calendarId: "1",
+          title: "my schedule",
+          category: "time",
+          dueDateClass: "",
+          start: "2018-12-18T22:30:00+09:00",
+          end: "2018-12-19T02:30:00+09:00"
+        },
+        {
+          id: "2",
+          calendarId: "1",
+          title: "second schedule",
+          category: "time",
+          dueDateClass: "",
+          start: "2018-12-18T17:30:00+09:00",
+          end: "2018-12-19T17:31:00+09:00"
+        }
+      ]
     };
   },
-  mounted() {
-  },
+  mounted() {},
   methods: {
-   onClickDayname(e){
-       debugger
-   },
-   onClickSchedule(e) {
-
+    onAfterRenderSchedule(e) {
+      // implement your code
+      debugger;
     },
+    onBeforeCreateSchedule(e) {
+      let params = {
+        id: this.scheduleList.length + 1,
+        calendarId: "1",
+        category: "time",
+        dueDateClass: "",
+        start: e.start,
+        title: e.title
+      };
+      this.scheduleList.push(params);
+    },
+    onBeforeDeleteSchedule(e) {
+      // implement your code
+      debugger;
+    },
+    onBeforeUpdateSchedule(e) {
+      // implement your code
+      debugger;
+    },
+    onClickDayname(e) {
+      // implement your code
+      debugger;
+    },
+    onClickSchedule(e) {
+      // implement your code
+      debugger;
+    },
+    onClickTimezonesCollapseBtn(e) {
+      // implement your code
+      debugger;
+    }
   }
 };
 </script>
