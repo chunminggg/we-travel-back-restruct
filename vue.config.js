9cc8eac4c85930dab69986834ef58962e228201f
neo-mac <miaobingliang@neotrident.com>
2019-04-22 13:40:07 +0800

    done

---
+++ vue.config.js
@@ -0,0 +1,18 @@
+const path = require('path')
+
+const resolve = dir => {
+  return path.join(__dirname, dir)
+}
+
+
+module.exports = {
+  // tweak internal webpack configuration.
+  // see https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
+  // 如果你不需要使用eslint，把lintOnSave设为false即可
+  lintOnSave: false,
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', resolve('src')) // key,value自行定义，比如.set('@@', resolve('src/components'))
+      .set('_c', resolve('src/components'))
+  },
+}
